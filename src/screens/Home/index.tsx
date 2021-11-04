6750b32b4e8d407d44c46b1b29e0448790ea79e0
Pablo <pablorsantos15@gmail.com>
2021-11-04 09:29:17 -0300

    chore: setup

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from './styles';
+
+export const Home: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Title>Home</S.Title>
+    </S.Container>
+  );
+};
+
