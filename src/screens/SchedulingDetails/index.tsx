e076a36e3488af7a56d21eff15c176bcd904b115
Pablo <pablorsantos15@gmail.com>
2021-11-08 23:42:31 -0300

    feat: scheduling details screen

---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Accessory } from "../../components/Accessory";
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
+import speedSvg from '../../assets/speed.svg'
+import accelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
+import * as S from "./styles";
+import { Button } from "../../components/Button";
+import { Feather } from '@expo/vector-icons'
+import { RFValue } from "react-native-responsive-fontsize";
+import { theme } from "../../styles/theme";
+import { useTheme } from "styled-components/native";
+
+export const SchedulingDetails: React.FC = () => {
+  const theme = useTheme()
+
+  return (
+    <S.Container>
+      <S.Header>
+        <BackButton onPress={() => {}} />
+      </S.Header>
+
+      <S.CarImages>
+        <ImageSlider
+          imagesUrl={["https://pngimg.com/uploads/audi/audi_PNG99491.png"]}
+        />
+      </S.CarImages>
+
+      <S.Content>
+        <S.Details>
+          <S.Description>
+            <S.Brand>Lamborghini</S.Brand>
+            <S.Name>Huracan</S.Name>
+          </S.Description>
+
+          <S.Rent>
+            <S.Period>Ao dia</S.Period>
+            <S.Price>R$ 280</S.Price>
+          </S.Rent>
+        </S.Details>
+
+        <S.Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </S.Accessories>
+
+        <S.RentalPeriod>
+          <S.CalendarIcon>
+            <Feather name="calendar" size={RFValue(24)} color={theme.colors.shape} />
+          </S.CalendarIcon>
+
+          <S.DateInfo>
+            <S.DateTitle>DE</S.DateTitle>
+            <S.DateValue>18/06/2021</S.DateValue>
+          </S.DateInfo>
+
+          <Feather name="chevron-right" size={RFValue(10)} color={theme.colors.text} />
+
+          <S.DateInfo>
+            <S.DateTitle>ATÉ</S.DateTitle>
+            <S.DateValue>18/06/2021</S.DateValue>
+          </S.DateInfo>
+        </S.RentalPeriod>
+     
+        <S.RentalPrice>
+          <S.RentalPriceLabel>TOTAL</S.RentalPriceLabel>
+          <S.RentalPriceDetails>
+            <S.RentalPriceQuota>R$ 580 x3 diárias</S.RentalPriceQuota>
+            <S.RentalPriceTotal>R$ 2.900</S.RentalPriceTotal>
+          </S.RentalPriceDetails>
+        </S.RentalPrice>
+      </S.Content>
+
+      <S.Footer>
+        <Button title="Confirmar" />
+      </S.Footer>
+    </S.Container>
+  );
+};
