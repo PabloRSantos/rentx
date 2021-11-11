e36afc2d69c6ef5ed60a06f2c6d6cae70c810bcb
Pablo <pablorsantos15@gmail.com>
2021-11-10 22:26:57 -0300

    feat: image slider

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import * as S from './styles';
+import React, { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
+import * as S from "./styles";
 
 interface Props {
-  imagesUrl: string[]
+  imagesUrl: string[];
+}
+
+interface ChangeImageProps {
+    viewableItems: ViewToken[];
+    changed: ViewToken[];
 }
 
 export const ImageSlider: React.FC<Props> = ({ imagesUrl }) => {
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    setImageIndex(info.viewableItems[0].index!)
+  })
+
   return (
     <S.Container>
-    <S.ImageIndexes>
-      <S.ImageIndex active={true} />
-      <S.ImageIndex active={false} />
-      <S.ImageIndex active={false} />
-      <S.ImageIndex active={false} />
-    </S.ImageIndexes>
-    
-    <S.CarImageWrapper>
-      <S.CarImage source={{ uri: imagesUrl[0] }} resizeMode="contain" />
-    </S.CarImageWrapper>
-    </S.Container>
+      <S.ImageIndexes>
+        {imagesUrl.map((_, index) => (
+          <S.ImageIndex key={String(index)} active={index === imageIndex} />
+        ))}
+      </S.ImageIndexes>
 
+      <FlatList
+        data={imagesUrl}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(key) => key}
+        onViewableItemsChanged={indexChanged.current}
+        renderItem={({ item }) => (
+          <S.CarImageWrapper>
+            <S.CarImage source={{ uri: item }} resizeMode="contain" />
+          </S.CarImageWrapper>
+        )}
+      />
+    </S.Container>
   );
 };
-
