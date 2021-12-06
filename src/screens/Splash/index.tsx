9dd24cfe8c9e58fcc6d598879e35d90f7d95d55b
Pablo <pablorsantos15@gmail.com>
2021-12-06 16:10:54 -0300

    feat: first animation

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import * as S from './styles';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated'
+import { Button, Dimensions, StyleSheet } from 'react-native';
+
+const WIDTH = Dimensions.get('window').width
 
 export const Splash: React.FC = () => {
+  const animation = useSharedValue(0)
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: withTiming(animation.value, { duration: 500, easing: Easing.ease }) }
+    ]
+  }))
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * (WIDTH - 100)
+  }
+
   return (
     <S.Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+        <Button title="Mover" onPress={handleAnimationPosition} />
     </S.Container>
   );
 };
 
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+})
