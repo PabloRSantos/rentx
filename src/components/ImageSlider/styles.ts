4f805eef1b12ed0daaf83cfed7d509bfc4a96d66
Pablo <pablorsantos15@gmail.com>
2021-12-16 10:58:25 -0300

    chore: fast image

--- src/components/ImageSlider/styles.ts
+++ src/components/ImageSlider/styles.ts
@@ -1,6 +1,6 @@
 import { Dimensions } from "react-native";
+import FastImage from "react-native-fast-image";
 import styled from "styled-components/native";
-
 
 
 export const Container = styled.View`
@@ -21,7 +21,7 @@ export const CarImageWrapper = styled.View`
   align-items: center;
 `;
 
-export const CarImage = styled.Image`
+export const CarImage = styled(FastImage)`
   width: 280px;
   height: 132px;
 `;
